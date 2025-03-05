ff111fbf54072679f32960e3fe7b39a28dbe24d2
osama <om2951855@gmail.com>
2025-03-05 06:19:44 +0200

    finish feature section

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+
+
+
+
+function App() {
+    return (
+        <main className="overflow-hidden">
+            <Header />
+            <Hero />
+            <Features />
+        </main>
+    );
+}
+
+export default App;
