f51f47288d7f5154d9acbb5db9e8fdf7333124ad
osama <om2951855@gmail.com>
2025-06-11 18:35:58 +0300

    Add FAQ section with collapsible items and integrate react-slidedown

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
+import Faq from "./sections/Faq";
+import Testimonials from "./sections/Testimonials";
+import Download from "./sections/Download";
+import Footer from "./sections/Footer";
 
 
 
@@ -13,6 +17,10 @@ function App() {
             <Hero />
             <Features />
             <Pricing />
+            <Faq />
+            <Testimonials />
+            <Download />
+            <Footer />
         </main>
     );
 }
